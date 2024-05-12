590ca0e8b730572bdcf08fbf5e312055dc78605f
Ludovic Stolycia <ludovic.stolycia@pixel.services>
2024-05-12 10:29:06 +0200

    add swap

--- src/memory_info.rs
+++ src/memory_info.rs
@@ -5,14 +5,18 @@ use sysinfo::{System};
 pub struct MemInfo {
     pub total_memory: u64,
     pub used_memory: u64,
+    pub total_swap: u64,
+    pub used_swap: u64,
 }
 
 pub fn get_memory_info() -> Result<MemInfo, Box<dyn std::error::Error>> {
     let mut sys = System::new_all();
     sys.refresh_all();
     let mem_info = MemInfo {
-        total_memory: sys.total_memory() / 1024 / 1024,
-        used_memory: sys.used_memory() / 1024 / 1024,
+        total_memory: sys.total_memory() ,
+        used_memory: sys.used_memory() ,
+        total_swap: sys.total_swap(),
+        used_swap: sys.used_swap(),
     };
     Ok(mem_info)
 }
