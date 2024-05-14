da3ce6b31d7e953e81746f05706f34ad26ca7a86
Ludovic Stolycia <ludovic.stolycia.ext@athea.tech>
2024-05-14 12:26:08 +0200

    add option command line

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use serde::{Serialize};
 
 use std::time::{SystemTime, UNIX_EPOCH};
 
+use clap::Parser;
+
 mod memory_info ;
 use crate::memory_info::get_memory_info;
 use crate::memory_info::MemInfo;
@@ -20,14 +22,27 @@ mod process_info ;
 use crate::process_info::get_proc_info;
 use crate::process_info::ProcInfo;
 
+#[derive(Parser)]
+#[clap(author, version, about = "Monitor system resources")]
+struct Args {
+    #[clap(short = 'd', long = "disk", help = "Include disk information")]
+    include_disk: bool,
+    #[clap(short = 'm', long = "memory", help = "Include memory information")]
+    include_memory: bool,
+    #[clap(short = 'n', long = "net", help = "Include network information")]
+    include_net: bool,
+    #[clap(short = 'p', long = "proc", help = "Include process information")]
+    include_proc: bool,
+}
+
 
 #[derive(Serialize)]
     struct Monitor {
         timestamp: u64,
-        disk_info: Vec<DiskInfo>,
-        memory_info: MemInfo,
-        net_info: Vec<NetInfo>,
-        proc_info: Vec<ProcInfo>,
+        disk_info: Option<Vec<DiskInfo>>,
+        memory_info: Option<MemInfo>,
+        net_info: Option<Vec<NetInfo>>,
+        proc_info: Option<Vec<ProcInfo>>,
     }
 
 
@@ -36,12 +51,42 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let now = SystemTime::now().duration_since(UNIX_EPOCH)?;
     let timestamp = now.as_millis() as u64;
 
+    let mut disk_info: Option<Vec<DiskInfo>> = None;
+    let mut memory_info: Option<MemInfo> = None;
+    let mut net_info: Option<Vec<NetInfo>> = None;
+    let mut proc_info: Option<Vec<ProcInfo>> = None;
+
+        let args = Args::parse(); // Parses command-line arguments
+
+
+    if args.include_disk {
+        // Get disk information using sysinfo or other libraries
+        disk_info = Some(get_disk_info()?) ;
+    }
+
+    if args.include_memory {
+        memory_info = Some(get_memory_info()?) ;
+    }
+
+    if args.include_net {
+        // Get network information using sysinfo or other libraries
+        net_info = Some(get_net_info()?) ;
+    }
+
+    if args.include_proc {
+        // Get process information using sysinfo
+        proc_info = Some(get_proc_info()?);
+    }
+
+
+
+
     let sysmonitor = Monitor {
         timestamp,
-        memory_info: get_memory_info()?,
-        disk_info: get_disk_info()?,
-        net_info: get_net_info()?,
-        proc_info: get_proc_info()?,
+        memory_info,
+        disk_info ,
+        net_info ,
+        proc_info ,
     };
 
     // Convert MemInfo to JSON
