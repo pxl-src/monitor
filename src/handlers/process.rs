610dae2ccc1077531d20cda8f7155ca6306f2b42
Ludovic Stolycia <ludovic.stolycia.ext@athea.tech>
2024-06-27 17:12:21 +0200

    add logging

--- src/handlers/process.rs
+++ src/handlers/process.rs
@@ -1,7 +1,9 @@
 use hyper::{Body, Response};
 use serde::Serialize;
 use sysinfo::{ProcessExt, System, SystemExt};
-use std::io;
+use std::io::{self, BufRead};
+use std::fs::File;
+use users::get_user_by_uid;
 
 #[derive(Serialize)]
 struct ProcessInfo {
@@ -10,11 +12,33 @@ struct ProcessInfo {
     cpu_usage: f32,
     memory: u64,
     virtual_memory: u64,
+    owner: String,
+}
+
+fn get_process_owner(pid: i32) -> String {
+    let status_path = format!("/proc/{}/status", pid);
+    if let Ok(file) = File::open(status_path) {
+        for line in io::BufReader::new(file).lines() {
+            if let Ok(line) = line {
+                if line.starts_with("Uid:") {
+                    let parts: Vec<&str> = line.split_whitespace().collect();
+                    if parts.len() > 1 {
+                        if let Ok(uid) = parts[1].parse::<u32>() {
+                            if let Some(user) = get_user_by_uid(uid) {
+                                return user.name().to_string_lossy().to_string();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    "Unknown".to_string()
 }
 
 pub async fn get_process_info() -> io::Result<Response<Body>> {
     let mut sys = System::new_all();
-    sys.refresh_all(); // Refresh the system to get updated information
+    sys.refresh_all();
 
     let process_infos: Vec<ProcessInfo> = sys.processes().iter().map(|(_, process)| {
         ProcessInfo {
@@ -23,6 +47,7 @@ pub async fn get_process_info() -> io::Result<Response<Body>> {
             cpu_usage: process.cpu_usage(),
             memory: process.memory(),
             virtual_memory: process.virtual_memory(),
+            owner: get_process_owner(i32::from(process.pid())),
         }
     }).collect();
 
